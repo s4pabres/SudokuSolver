450c6b04ca7bb1ad3f9a65027c90c1a53f0856f0
Nico Feld <nico.feld@etis-gmbh.de>
2018-11-21 21:43:25 +0100

    first commit

--- SudokuSolver/Data/Message.cs
+++ SudokuSolver/Data/Message.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace SudokuSolver.Data
 
         public string text = "";
 
+        public string type;
     }
 }
