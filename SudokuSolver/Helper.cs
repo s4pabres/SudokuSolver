a8597ad53a36f3221cde46057b4c38795f1eb519
Nico Feld <nico.feld@etis-gmbh.de>
2018-11-21 22:36:38 +0100

    first real commit

---
+++ SudokuSolver/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Helper
+    {
+        public static string[] GetAllLocalIPv4(NetworkInterfaceType _type)
+        {
+            List<string> ipAddrList = new List<string>();
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            ipAddrList.Add(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+            return ipAddrList.ToArray();
+        }
+
+    }
+}
--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -10,7 +10,22 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            string Name = args[0];
+            string ManagerURI = args[2];
+            ushort[,] Cells = new ushort[3,3];
+            foreach (var cell in args[1].Split(','))
+            {
+                Cells[(ushort)(cell[0] - '0'), (ushort) (cell[1] - '0')] = (ushort) (cell[3] -'0');
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(Cells[i,j]);
+                }
+            }
+
             Console.ReadKey();
         }
     }
