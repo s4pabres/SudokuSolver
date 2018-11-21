f28477d5a750e098ff1c208227fcf32a42a9a120
Nico Feld <nico.feld@etis-gmbh.de>
2018-11-21 21:38:53 +0100

    Projektdateien hinzufügen.

---
+++ SudokuSolver/Data/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Data
+{
+    class Member
+    {
+        public Uri Uri { get; }
+        public string Name { get; }
+
+
+    }
+}
---
+++ SudokuSolver/Data/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Data
+{
+    class Message
+    {
+        public Member source { get; }
+        public Member target { get; }
+
+        public string text = "";
+
+    }
+}
