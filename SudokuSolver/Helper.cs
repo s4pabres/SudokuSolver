fc4413ea75a8bba31f45cf94e1a637c7457e9ac7
Cataract92 <salatgurke92@hotmail.de>
2018-11-22 00:31:15 +0100

    few stuff

--- SudokuSolver/Helper.cs
+++ SudokuSolver/Helper.cs
@@ -10,6 +10,19 @@ namespace SudokuSolver
 {
     class Helper
     {
+        public static readonly Dictionary<string, string[]> Neighbours = new Dictionary<string, string[]>()
+        {
+            {"BOX_A1", new[] { "BOX_D1", "BOX_A3" }},
+            {"BOX_D1", new[] { "BOX_A1", "BOX_G1", "BOX_D4" }},
+            {"BOX_G1", new[] { "BOX_D1", "BOX_G4" }},
+            {"BOX_A4", new[] { "BOX_A1", "BOX_D4", "BOX_A7" }},
+            {"BOX_D4", new[] { "BOX_D1", "BOX_A4", "BOX_G4", "BOX_D7" }},
+            {"BOX_G4", new[] { "BOX_G1", "BOX_D4", "BOX_G7" }},
+            {"BOX_A7", new[] { "BOX_A1", "BOX_G1", "BOX_D4" }},
+            {"BOX_D7", new[] { "BOX_D4", "BOX_A7", "BOX_G7" }},
+            {"BOX_G7", new[] { "BOX_G4", "BOX_D7" }},
+        };
+
         public static string[] GetAllLocalIPv4(NetworkInterfaceType _type)
         {
             List<string> ipAddrList = new List<string>();
@@ -26,7 +39,15 @@ namespace SudokuSolver
                     }
                 }
             }
+
             return ipAddrList.ToArray();
+        }
+
+        public static void SendMessage(TcpClient Client, string message)
+        {
+            byte[] bytes =
+                Encoding.ASCII.GetBytes(message);
+            Client.GetStream().Write(bytes, 0, bytes.Length);
         }
 
     }
--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,26 +10,31 @@ namespace SudokuSolver
 {
     class Program
     {
+        private const ushort Port = 8080;
+
+      
+
         static void Main(string[] args)
         {
             string Name = args[0];
-            string ManagerURI = args[2];
-            ushort[,] Cells = new ushort[3,3];
+            ushort[,] Cells = new ushort[3, 3];
             foreach (var cell in args[1].Split(','))
             {
-                Cells[(ushort)(cell[0] - '0'), (ushort) (cell[1] - '0')] = (ushort) (cell[3] -'0');
+                Cells[(ushort)(cell[0] - '0'), (ushort)(cell[1] - '0')] = (ushort)(cell[3] - '0');
             }
 
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(Cells[i,j]);
-                }
-                Console.WriteLine();
-            }
+            TcpClient Manager = new TcpClient();
+            Manager.Connect(args[2].Split(':')[1].Substring(2), Int32.Parse(args[2].Split(':')[2]));
+
+            Helper.SendMessage(Manager, Name + "," + Helper.GetAllLocalIPv4(NetworkInterfaceType.Ethernet)[0] + "," +
+                                        Port);
+
+            Dictionary<string, TcpClient> Neighbours = new Dictionary<string, TcpClient>();
+            
+
 
             Console.ReadKey();
         }
+
     }
 }
